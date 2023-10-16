4c897558f5eae44dce6a932f027967b2e4b32a0b
root <romaana.k7@gmail.com>
2023-10-16 10:29:40 -0700

    shell

--- shell.h
+++ shell.h
@@ -93,6 +93,70 @@ typedef struct pass
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
+/**
+ * struct builtin - builtin string funcs
+ * @type: builtin cmd
+ * @func: function type
+ */
+typedef struct builtin
+{
+	char *type;
+	int (*func)(info_t *);
+} builtin_table;
 
+/* shell_loop.c */
+int hsh(info_t *, char **);
+int find_builtin(info_t *);
+void find_cmd(info_t *);
+void fork_cmd(info_t *);
+
+/* parser.c */
+int is_cmd(info_t *, char *);
+char *dupl_chars(char *, int, int);
+char *find_path(info_t *, char *, char *);
+
+/* loophsh.c */
+int loophsh(char **);
+
+/* _errors.c */
+void _eputs(char *);
+int _eputchar(char);
+int _putfd(char c, int fd);
+int putsfd(char *str, int fd);
+
+/* _string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* _string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* _exits.c */
+char *_strncpy(char *, char *, int);
+char _strncat(char *, char *, int);
+char *_strchr(char *, char);
+
+/* token.c */
+char **strtow(char *, char *);
+char **strtow2(char *, char);
+
+/* realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
+
+/* mem.c */
+int bfree(void **);
+
+/* _atoi.c */
+int interactive(info_t *);
+int is_delim(char, char *);
+int _atoi(char *);
+int _isalpha(int);
 
 #endif
