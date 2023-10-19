8a69d2702955f9b0d4f413b095af6d420da2a390
root <romaana.k7@gmail.com>
2023-10-18 19:12:46 -0700

    errs

--- err1.c
+++ err1.c
@@ -38,12 +38,102 @@ void print_error(info_t *info, char *stre)
 {
 	_eputs(info->file_name);
 	_eputs(": ");
-	d_prints(info->line_count, STDERR_FILENO);
+	_prints(info->line_count, STDERR_FILENO);
 	_eputs(": ");
 	_eputs(info->argv[0]);
 	_eputs(": ");
 	_eputs(stre);
 }
 
+/**
+ * _prints - prints decimal 
+ * @in: input
+ * @fd: fd
+ *
+ * return: no. of chars
+ */
+int _prints(int in, int fd)
+{
+	int (*_putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int absolute, current;
 
+	if (fd == STDERR_FILENO)
+		_putchar = eputchar;
+	if (in < 0)
+	{
+		absolute = -in;
+		_putchar('-');
+		count++;
+	}
+	else
+		absolute = in;
+	current = absolute;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (absolute / i)
+		{
+			_putchar('0' + current / i);
+			count__;
+		}
+		current %= i
+	}
+	_putchar('0' + current);
+	count++;
+
+	return (count);
+}
+
+/**
+ * convert_no - convert to itoa
+ * @num: num
+ * @b: base
+ * @flag: arg flag
+ *
+ * return: str
+ */
+char *convert_no(long int num, int b, int flag)
+{
+	static char *array;
+	static char buffer[50];
+	char s = 0;
+	char *ptr;
+	unsigned long n = num;
+
+	if (!(flag & CONVERT_UNSIGNED) && num < 0)
+	{
+		n = num;
+		s = '-';
+	}
+	array = flag & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % b];
+		n /= b;
+	} while (n != 0);
+
+	if (s)
+		*--ptr = s;
+	return (ptr);
+}
+
+/**
+ * remove_comm - func rep # with \0
+ * @buf: addr of str
+ *
+ * return: 0
+ *
+ */
+void remove_comm(char *buf)
+{
+	int i;
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '# && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
 
